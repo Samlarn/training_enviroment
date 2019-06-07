97bb0898a941991bd4f9e26a37bc1884111e1a57
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2019-06-08 00:41:18 +0200

    prepare images function, and now return either zipDataset and/or mapDataset

--- TrainingEnviroment/DataPipeline/imagePipline.py
+++ TrainingEnviroment/DataPipeline/imagePipline.py
@@ -14,7 +14,7 @@ width = 120
 height = 120
 channels = 3
 
-def setup_and_get_zipDataset(path_to_images, width, height, channels=3):
+def prepare_images(path_to_images, width, height, channels=3):
     width = width
     height = height
     data_root = pathlib.Path(path_to_images)
@@ -23,16 +23,27 @@ def setup_and_get_zipDataset(path_to_images, width, height, channels=3):
     random.shuffle(all_image_paths)
     image_count = len(all_image_paths)
     label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
-
     label_to_index = dict((name, index) for index,name in enumerate(label_names))
     all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
 
+
+    return all_image_paths, all_image_labels, image_count
+
+
+def get_zip_dataset(all_image_paths, all_image_labels):
     path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
     image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64))
+    return tf.data.Dataset.zip((image_ds, label_ds))
 
-    image_label_ds_zipDataset = tf.data.Dataset.zip((image_ds, label_ds))
-    return image_label_ds_zipDataset
+
+def get_map_dataset(all_image_paths, all_image_labels):
+    ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_labels))
+    return ds.map(load_and_preprocess_from_path_label)
+
+
+def load_and_preprocess_from_path_label(path, label):
+  return load_and_preprocess_image(path), label
 
 
 def preprocess_image(image):
--- TrainingEnviroment/main.py
+++ TrainingEnviroment/main.py
@@ -7,6 +7,15 @@ import DataPipeline.imagePipline as ip
 
 local_path = "C:\\Users\\jonss\\Desktop\\ml\\workspaces\\data\\leafs"
 
-zip_dataset = ip.setup_and_get_zipDataset(local_path, 192, 192)
+all_image_paths, all_image_labels, image_count = ip.prepare_images(local_path, 192, 192)
 
-print(zip_dataset)
+print(image_count)
+print(all_image_paths[:5])
+print(all_image_labels)
+
+zipDataset = ip.get_zip_dataset(all_image_paths, all_image_labels)
+print(zipDataset)
+
+
+mapDataset = ip.get_map_dataset(all_image_paths, all_image_labels)
+print(mapDataset)
